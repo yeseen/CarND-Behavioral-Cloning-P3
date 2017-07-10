11c07119a90fc0764b1e695193b5de72102c6317
Yassine Dhane <yassinedhane@gmail.com>
2017-07-10 20:24:28 +0000

    trained modelX1

--- train.py
+++ train.py
@@ -12,13 +12,17 @@ with open('dataX/driving_log.csv') as csvfile:
 		c_path = 'dataX/IMG/' + fname
 		print(c_path)
 		image = cv2.imread(c_path)
-		#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-		images.append(image)
-		angles.append(float(line[3]))
-		#image = cv2.flip(image,1)
-		image = np.fliplr(image)
-		images.append(image)
-		angles.append( - float(line[3]))
+		try :
+			image_flip = np.fliplr(image)
+			#image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+			images.append(image)
+			angles.append(float(line[3]))
+			#image = cv2.flip(image,1)
+			images.append(image_flip)
+			angles.append( - float(line[3]))
+		except :
+		 	pass
+
 
 X_train = np.array(images)
 y_train = np.array(angles)
@@ -50,7 +54,7 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=2)
 
 model.save('modelX1.h5')
 exit()
